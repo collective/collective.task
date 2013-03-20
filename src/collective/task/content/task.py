b2c92c61a31f93db7f1197d55be0efe49efc9f58
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-20 16:41:48 +0100

    Added responsible field to task

--- src/collective/task/content/task.py
+++ src/collective/task/content/task.py
@@ -4,12 +4,24 @@ from zope import schema
 from plone.dexterity.content import Container
 from plone.supermodel import model
 
+from collective.dms.basecontent._field import LocalRolesToPrincipals
+
 from collective.task import _
 
 
 class ITask(model.Schema):
     title = schema.TextLine(title=_(u'Title'))
-    deadline = schema.Date(title=_(u'Date'), required=False)
+    deadline = schema.Date(title=_(u'Date'),
+                           required=False)
+
+    responsible = LocalRolesToPrincipals(
+        title=_(u"Responsible"),
+        roles_to_assign=('Editor',),
+        required=False,
+        value_type=schema.Choice(
+            vocabulary="plone.principalsource.Principals"
+        ),
+    )
 
 
 class Task(Container):
