64f6f9364cbb13fc336202a005f22a3736d1d04e
Frédéric Péters <fpeters@entrouvert.com>
2013-03-20 10:27:30 +0100

    add minimalistic task type

---
+++ src/collective/task/content/task.py
@@ -0,0 +1,15 @@
+from zope.interface import implements
+from zope import schema
+
+from plone.dexterity.content import Container
+from plone.supermodel import model
+
+from collective.task import _
+
+class ITask(model.Schema):
+    title = schema.TextLine(title=_(u'Title'))
+    deadline = schema.Date(title=_(u'Date'))
+
+
+class Task(Container):
+    implements(ITask)
