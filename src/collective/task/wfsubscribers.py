0b7d6ea874f0dab8aff34d259040caff5398530f
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-21 16:13:18 +0100

    Added subscriber to change parent task state when subtask state changed

---
+++ src/collective/task/wfsubscribers.py
@@ -0,0 +1,20 @@
+from five import grok
+
+from Products.DCWorkflow.interfaces import IAfterTransitionEvent
+
+from collective.task.content.task import ITask
+from plone import api
+
+
+@grok.subscribe(ITask, IAfterTransitionEvent)
+def task_changed_state(context, event):
+    """When a task is abandoned or done, check if it is a subtask
+    and make the wanted transition to parent
+    """
+    parent = context.getParentNode()
+    if parent.portal_type == 'task':
+        with api.env.adopt_roles(['Reviewer']):
+            if event.new_state.id == 'done':
+                api.content.transition(obj=parent, transition='subtask-done')
+            elif event.new_state.id == 'abandoned':
+                api.content.transition(obj=parent, transition='subtask-abandoned')
