a1445dcdc97e9aeb4c908d0658a78c75711a2e93
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-26 09:35:11 +0100

    Set parent deadline and title when attributing a task

--- src/collective/task/browser/attribute_task.py
+++ src/collective/task/browser/attribute_task.py
@@ -1,3 +1,5 @@
+from five import grok
+
 from z3c.form import button
 from z3c.form.field import Fields
 from z3c.form.interfaces import HIDDEN_MODE
@@ -40,6 +42,11 @@ class AttributeTask(DefaultAddForm):
         if 'workflow_action' in self.request:
             self.widgets['workflow_action'].value = (
                 self.request['workflow_action'])
+        self.widgets['title'].value = self.context.title
+        deadline = (str(self.context.deadline.year),
+                    str(self.context.deadline.month),
+                    str(self.context.deadline.day))
+        self.widgets['deadline'].value = deadline
 
     @button.buttonAndHandler(_('Add'), name='save')
     def handleAdd(self, action):
