476fed995b69862946784f078b350f5c7e4942e0
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-27 17:49:21 +0100

    Added tests for content types

---
+++ src/collective/task/tests/test_content.py
@@ -0,0 +1,34 @@
+import datetime
+
+from plone import api
+
+from ecreall.helpers.testing.base import BaseTest
+
+from ..testing import IntegrationTestCase
+
+
+class TestContentTypes(IntegrationTestCase, BaseTest):
+    """Tests content types"""
+
+    def setUp(self):
+        super(TestContentTypes, self).setUp()
+        portal = api.portal.get()
+        folder = portal['folder']
+        params = {'type': 'task',
+                  'title': u'My task',
+                  'responsible': ['bigboss'],
+                  'deadline': datetime.date(2013, 11, 2),
+                  }
+        folder.invokeFactory('task', "my-task", **params)
+        self.mytask = folder['my-task']
+
+    def test_add_task(self):
+        portal = api.portal.get()
+        folder = portal['folder']
+        self.assertIn('my-task', folder)
+
+    def test_task_fields(self):
+        mytask = self.mytask
+        self.assertEqual(u'My task', mytask.Title())
+        self.assertEqual(datetime.date(2013, 11, 2), mytask.deadline)
+        self.assertEqual(['bigboss'], mytask.responsible)
