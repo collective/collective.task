189e583396cb86575db239f1270e67b3052bc21c
Cédric Messiant <cedricmessiant@ecreall.com>
2013-04-06 19:24:26 +0200

    Fixed attribute_task

--- src/collective/task/browser/attribute_task.py
+++ src/collective/task/browser/attribute_task.py
@@ -5,18 +5,29 @@ from z3c.form.field import Fields
 from z3c.form.interfaces import HIDDEN_MODE
 from zope import schema
 
-from Acquisition import aq_inner
+from Acquisition import aq_inner, aq_chain
 
 from plone import api
 from plone.dexterity.browser.add import DefaultAddForm
 from plone.dexterity.i18n import MessageFactory as DMF
 from plone.supermodel import model
 
+from Products.CMFPlone.utils import base_hasattr
 from Products.statusmessages.interfaces import IStatusMessage
 
 from collective.task.content.task import ITask
 
 from collective.task import _
+
+
+def find_nontask(obj):
+    """Find the first non task object in acquisition chain"""
+    for item in aq_chain(aq_inner(obj)):
+        if base_hasattr(item, 'portal_type'):
+            if item.portal_type != 'task':
+                return item
+        else:
+            return item
 
 
 class IWorkflowAction(model.Schema):
@@ -55,7 +66,8 @@ class AttributeTask(DefaultAddForm):
         grant Reviewer role to current user on new object
         Then, execute transition on container
         """
-        container_url = aq_inner(self.context).absolute_url()
+        parent_task = aq_inner(self.context)
+        container_url = parent_task.absolute_url()
         data, errors = self.extractData()
         workflow_action = data['workflow_action']
         del data['workflow_action']
@@ -64,10 +76,13 @@ class AttributeTask(DefaultAddForm):
             return
         obj = self.createAndAdd(data)
         if obj is not None:
+            supervisor = parent_task.responsible[0]
             # mark only as finished if we get the new object
             self._finishedAdd = True
             IStatusMessage(self.request).addStatusMessage(DMF(u"Item created"), "info")
             # set Reviewer role on new object to the current user
-            parent_task_editor = api.user.get_current()
-            api.user.grant_roles(user=parent_task_editor, obj=obj, roles=['Reviewer',])
+            obj.manage_setLocalRoles(supervisor, ['Reviewer',])
+            # set Editor role to task responsible on the first non Task object in acquisition
+            nontask = find_nontask(parent_task)
+            nontask.manage_setLocalRoles(obj.responsible[0], ['Editor',])
             self.immediate_view = "%s/content_status_modify?workflow_action=%s" % (container_url, workflow_action)
