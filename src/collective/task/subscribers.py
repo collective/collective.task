2e7bd0cb1a79a3c966e28dea1be1ce842dead3ff
Cédric Messiant <cedricmessiant@ecreall.com>
2013-05-28 14:32:18 +0200

    Fix bug when deleting content with multiple subtasks

--- src/collective/task/subscribers.py
+++ src/collective/task/subscribers.py
@@ -12,6 +12,7 @@ def reopen_parent_task(context, event):
     """When a task is deleted, reopen its parent task
     """
     parent = context.getParentNode()
-    if parent.portal_type == 'task':
+    parent_state = api.content.get_state(parent)
+    if parent.portal_type == 'task' and parent_state != 'todo':
         with api.env.adopt_roles(['Reviewer']):
             api.content.transition(obj=parent, transition='subtask-abandoned')
