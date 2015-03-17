9bc1c6713970eb41afab93ee4bb81c0058f2d7a0
Stephan Geulette <s.geulette@imio.be>
2015-03-17 16:43:32 +0100

    Don't rebuild to keep manually msgid

--- src/collective/task/locales/update.sh
+++ src/collective/task/locales/update.sh
@@ -1,5 +1,5 @@
 domain=collective.task
-i18ndude rebuild-pot --pot $domain.pot --create $domain ../
+#i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
 
 i18ndude rebuild-pot --pot plone.pot --create plone ../profiles
