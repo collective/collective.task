b576786a7f50d9dcb21b11d1352c34b00877fcb9
Stephan Geulette <s.geulette@imio.be>
2017-03-30 09:25:31 +0200

    change widget to hide it, because default widget (orderedselect) doesn't have hidden widget template

--- src/collective/task/interfaces.py
+++ src/collective/task/interfaces.py
@@ -4,6 +4,7 @@
 from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from z3c.form.browser.select import SelectFieldWidget
 from plone.app.dexterity import PloneMessageFactory as _PMF
 from plone.autoform import directives
 from plone.supermodel import model
@@ -30,14 +31,18 @@ class ITaskContent(model.Schema):
         required=False,
         value_type=schema.Choice(vocabulary=u'collective.task.AssignedGroups')
     )
-    #directives.mode(parents_assigned_groups='hidden')
+    # must change widget to hide it, because default widget (orderedselect) doesn't have hidden widget template
+    directives.widget('parents_assigned_groups', SelectFieldWidget, multiple='multiple', size=10)
+    directives.mode(parents_assigned_groups='hidden')
 
     parents_enquirers = LocalRolesField(
         title=_(u"Parents enquirers"),
         required=False,
         value_type=schema.Choice(vocabulary=u'collective.task.Enquirer')
     )
-    #directives.mode(parents_enquirers='hidden')
+    # must change widget to hide it, because default widget (orderedselect) doesn't have hidden widget template
+    directives.widget('parents_enquirers', SelectFieldWidget, multiple='multiple', size=10)
+    directives.mode(parents_enquirers='hidden')
 
 
 class ITaskMethods(Interface):
