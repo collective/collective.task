6fc6ae28e301ef8ca2906ae22cbcb3b988998759
Simon Delcourt <delcourt.simon@gmail.com>
2017-04-13 16:39:40 +0200

    Register field handler to be able to tune it through dexterity xml model declaration.

--- src/collective/task/field.py
+++ src/collective/task/field.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 """Define custom fields."""
-from plone.formwidget.masterselect import MasterSelectField
 
 from dexterity.localrolesfield.field import LocalRoleField
+
+from plone.formwidget.masterselect import MasterSelectField
+import plone.supermodel.exportimport
 
 
 class LocalRoleMasterSelectField(LocalRoleField, MasterSelectField):
 
     """Local role single value field which can be used as a master select."""
+
+LocalRoleSelectHandler = plone.supermodel.exportimport.ChoiceHandler(LocalRoleMasterSelectField)
+LocalRoleHandler = plone.supermodel.exportimport.ChoiceHandler(LocalRoleField)
